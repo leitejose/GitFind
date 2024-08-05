8cfe4112d11abf2430a482a14fea6b318f824d27
José Leite <joseleite688@gmail.com>
2024-08-05 19:17:42 +0100

    Adicionar novos arquivos

---
+++ src/pages/Home/index.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { Header } from '../../components/Header';
+import background from '../../assets/background.png';
+import ItemList from '../../components/ItemList';
+
+import './styles.css';
+
+function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="content">
+        <img src={background} alt="background" className="background" />
+        <div className="info">
+          <div>
+            <input
+              name="utilizador"
+              value={user}
+              onChange={event => setUser(event.target.value)}
+              placeholder="@username"
+            />
+
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {currentUser?.name ? (
+            <>
+              <div className="profile">
+                <img src={currentUser.avatar_url} className="profile-image" alt="avatar" />
+                <div className="profile-info">
+                  <h3>{currentUser.name}</h3>
+                  <span>{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>
+          ) : null}
+          {repos?.length ? (
+            <div className="repos">
+              <h4 className='repositories'>Repositórios</h4>
+              {repos.map(repo => (
+                <ItemList
+                  key={repo.id}  // Adicione uma chave única para cada item da lista
+                  title={repo.name}
+                  description={repo.description} />
+              ))}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
